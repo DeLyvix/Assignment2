6a04514a4b9b0089a4e54feb09613b9aac9005e7
DeLyvix <32754061+DeLyvix@users.noreply.github.com>
2017-11-26 23:31:14 +0800

    Update packet4.py

--- packet4.py
+++ packet4.py
@@ -1,43 +1,29 @@
 import socket, sys
-
 from struct import *
 
 source_ip = '192.168.159.129'
-
 dest_ip = '192.168.10.128' 
 
 try:
-
     s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_UDP)
-
 except socket.error , msg:
 
     print 'Socket could not be created. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
 
     sys.exit() 
 
-    packet = '';
+packet = '';
 
 ip_ihl = 5
-
 ip_ver = 4
-
 ip_tos = 0
-
 ip_tot_len = 0  
-
 ip_id = 54321   
-
 ip_frag_off = 0
-
 ip_ttl = 255
-
 ip_proto = socket.IPPROTO_UDP
-
 ip_check = 0    
-
 ip_saddr = socket.inet_aton ( source_ip )   
-
 ip_daddr = socket.inet_aton ( dest_ip )
 
 ip_ihl_ver = (4 << 4) | 5
@@ -47,18 +33,13 @@ ip_header = pack('!BBHHHBBH4s4s' , ip_ihl_ver, ip_tos, ip_tot_len, ip_id, ip_fra
 user_data = 'PYTHON PACKET TEST'
 
 source_address = socket.inet_aton( source_ip )
-
 dest_address = socket.inet_aton(dest_ip)
-
 placeholder = 0
 protocol = socket.IPPROTO_UDP
-
 length = len(user_data)
 
 psh = pack('!4s4sBBH' , source_address , dest_address , placeholder , protocol,length);
-
 psh = psh + user_data;
-
 packet = ip_header + user_data
 
 var = 1
